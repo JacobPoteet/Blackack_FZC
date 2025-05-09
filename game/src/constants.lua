4a0166a5e7a2b76f81fa3a8abb42449061cdedd5
Jacob Poteet <64765148+JacobPoteet@users.noreply.github.com>
2025-05-08 21:41:53 -0400

    Add constants folder for level files

---
+++ game/src/constants.lua
@@ -0,0 +1,10 @@
+local constants = {}
+
+constants.SCENES = {
+    MENU = "src.scenes.menu",
+    PLAY = "src.scenes.play",
+    SETTINGS = "src.scenes.settings",
+    EXIT = "src.scenes.exit"
+}
+
+return constants
